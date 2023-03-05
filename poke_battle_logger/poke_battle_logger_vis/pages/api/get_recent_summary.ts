9cb20600f87bd0408660fcac4462a9125783a8be
fufufukakaka <demonstrundum@gmail.com>
2023-03-05 18:44:17 +0900

    set api

--- poke_battle_logger/poke_battle_logger_vis/pages/api/get_recent_summary.ts
+++ poke_battle_logger/poke_battle_logger_vis/pages/api/get_recent_summary.ts
@@ -1,8 +1,9 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from "axios"
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ name: 'John Doe' })
+  const results = await axios.get("http://127.0.0.1:8000/api/v1/recent_battle_summary");
+  res.status(200).json(results.data)
 }
 
 export default handler
--- poke_battle_logger/poke_battle_logger_vis/pages/index.tsx
+++ poke_battle_logger/poke_battle_logger_vis/pages/index.tsx
@@ -19,83 +19,85 @@ import {
   useColorModeValue,
 } from '@chakra-ui/react';
 
-const menuItems = [
-  { label: 'Menu Item 1' },
-  { label: 'Menu Item 2' },
-  { label: 'Menu Item 3' },
-];
-
 const Dashboard = () => {
   return (
     <Box bg="gray.50" minH="100vh">
       <Container maxW="container.xl" py="8">
-          <Box flex="1" p="4" bg="white">
-            <Stack spacing={4}>
-              <StatGroup>
+        <Box flex="1" p="4" bg="white">
+          <Stack spacing={4}>
+            <StatGroup>
               <Stat
-      px={{ base: 4, md: 8 }}
-      py={'5'}
-      shadow={'s'}
-      rounded={'lg'}>
-                  <StatLabel fontWeight={'medium'}>勝率 👊</StatLabel>
-                  <StatNumber>54%</StatNumber>
-                  <StatHelpText>
-                    <StatArrow type="increase" />
-                    2%
-                  </StatHelpText>
-                </Stat>
-                <Stat
-      px={{ base: 4, md: 8 }}
-      py={'5'}
-      shadow={'s'}
-      rounded={'lg'}>
-                  <StatLabel fontWeight={'medium'}>順位 👑</StatLabel>
-                  <StatNumber>80900</StatNumber>
-                  <StatHelpText>
-                    <StatArrow type="decrease" />
-                    10000
-                  </StatHelpText>
-                </Stat>
-                <Stat
-      px={{ base: 4, md: 8 }}
-      py={'5'}
-      shadow={'s'}
-      rounded={'lg'}>
-                  <StatLabel fontWeight={'medium'}>最近勝ったポケモン ⭕</StatLabel>
-                  <StatNumber>ヘイラッシャ</StatNumber>
-                </Stat>
-                <Stat
-      px={{ base: 4, md: 8 }}
-      py={'5'}
-      shadow={'s'}
-      rounded={'lg'}>
-                  <StatLabel fontWeight={'medium'}>最近負けたポケモン ❎</StatLabel>
-                  <StatNumber>ハバタクカミ</StatNumber>
-                </Stat>
-              </StatGroup>
-              <Table>
-                <Thead>
-                  <Tr>
-                    <Th>Name</Th>
-                    <Th>Email</Th>
-                    <Th>Phone</Th>
-                  </Tr>
-                </Thead>
-                <Tbody>
-                  <Tr>
-                    <Td>John Doe</Td>
-                    <Td>john@example.com</Td>
-                    <Td>555-555-5555</Td>
-                  </Tr>
-                  <Tr>
-                    <Td>Jane Doe</Td>
-                    <Td>jane@example.com</Td>
-                    <Td>555-555-5555</Td>
-                  </Tr>
-                </Tbody>
-              </Table>
-            </Stack>
-          </Box>
+                px={{ base: 4, md: 8 }}
+                py={'5'}
+                shadow={'s'}
+                rounded={'lg'}
+              >
+                <StatLabel fontWeight={'medium'}>勝率 👊</StatLabel>
+                <StatNumber>54%</StatNumber>
+                <StatHelpText>
+                  <StatArrow type="increase" />
+                  2%
+                </StatHelpText>
+              </Stat>
+              <Stat
+                px={{ base: 4, md: 8 }}
+                py={'5'}
+                shadow={'s'}
+                rounded={'lg'}
+              >
+                <StatLabel fontWeight={'medium'}>順位 👑</StatLabel>
+                <StatNumber>80900</StatNumber>
+                <StatHelpText>
+                  <StatArrow type="decrease" />
+                  10000
+                </StatHelpText>
+              </Stat>
+              <Stat
+                px={{ base: 4, md: 8 }}
+                py={'5'}
+                shadow={'s'}
+                rounded={'lg'}
+              >
+                <StatLabel fontWeight={'medium'}>
+                  最近勝ったポケモン ⭕
+                </StatLabel>
+                <StatNumber>ヘイラッシャ</StatNumber>
+              </Stat>
+              <Stat
+                px={{ base: 4, md: 8 }}
+                py={'5'}
+                shadow={'s'}
+                rounded={'lg'}
+              >
+                <StatLabel fontWeight={'medium'}>
+                  最近負けたポケモン ❎
+                </StatLabel>
+                <StatNumber>ハバタクカミ</StatNumber>
+              </Stat>
+            </StatGroup>
+            <Table>
+              <Thead>
+                <Tr>
+                  <Th>Name</Th>
+                  <Th>Email</Th>
+                  <Th>Phone</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td>John Doe</Td>
+                  <Td>john@example.com</Td>
+                  <Td>555-555-5555</Td>
+                </Tr>
+                <Tr>
+                  <Td>Jane Doe</Td>
+                  <Td>jane@example.com</Td>
+                  <Td>555-555-5555</Td>
+                </Tr>
+              </Tbody>
+            </Table>
+          </Stack>
+        </Box>
       </Container>
     </Box>
   );
