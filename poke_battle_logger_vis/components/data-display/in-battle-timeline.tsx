43e3057a6a556f616ae0f856b457efec66f36d8a
fufufukakaka <demonstrundum@gmail.com>
2023-05-21 22:52:27 +0900

    show message log

--- poke_battle_logger_vis/components/data-display/in-battle-timeline.tsx
+++ poke_battle_logger_vis/components/data-display/in-battle-timeline.tsx
@@ -2,9 +2,14 @@ import { Chrono } from "react-chrono";
 import PokemonIcon from "../atoms/pokemon-icon";
 import { Box, Flex } from "@chakra-ui/react";
 import {
+  Badge,
   ListItem,
   OrderedList,
+  Switch,
+  FormControl,
+  FormLabel,
 } from '@chakra-ui/react'
+import { useState } from "react";
 
 interface InBattleTimelineProps {
   in_battle_log: {
@@ -21,35 +26,46 @@ interface InBattleTimelineProps {
 }
 
 const InBattleTimeline: React.FC<InBattleTimelineProps> = ({ in_battle_log, message_log }) => {
+  const [showMessage, setShowMessage] = useState(false);
+
   return (
-    <Chrono
-      mode="VERTICAL"
-      cardHeight={100}
-    >
-      {in_battle_log.map((log) => (
-        <Box key={log.turn}>
-          <h3>Turn: {log.turn}</h3>
-          <Flex alignItems={"flex-end"}>
-            <PokemonIcon
-              pokemon_name={log.your_pokemon_name}
-              boxSize={'50px'}
-            />
-            <p>VS</p>
-            <PokemonIcon
-              pokemon_name={log.opponent_pokemon_name}
-              boxSize={'50px'}
-            />
-          </Flex>
-          <OrderedList>
-            {
-              message_log.filter((message) => Number(message.turn) === Number(log.turn)).map((message) => (
-                <ListItem key={message.frame_number}>{message.message}</ListItem>
-              ))
-            }
-          </OrderedList>
-        </Box>
-      ))}
-    </Chrono>
+    <>
+      <FormControl display='flex' alignItems='center'>
+        <FormLabel htmlFor='show-message' mb='0'>
+          <Badge colorScheme='purple'>BETA</Badge> Show Battle Log Message
+        </FormLabel>
+        <Switch id='show-message' onChange={(e) => setShowMessage(!showMessage)} />
+      </FormControl>
+      <Chrono
+        mode="VERTICAL"
+        cardHeight={100}
+        allowDynamicUpdate={true}
+      >
+        {in_battle_log.map((log) => (
+          <Box key={log.turn}>
+            <h3>Turn: {log.turn}</h3>
+            <Flex alignItems={"flex-end"}>
+              <PokemonIcon
+                pokemon_name={log.your_pokemon_name}
+                boxSize={'50px'}
+              />
+              <p>VS</p>
+              <PokemonIcon
+                pokemon_name={log.opponent_pokemon_name}
+                boxSize={'50px'}
+              />
+            </Flex>
+            <OrderedList>
+              {
+                (message_log.filter((message) => Number(message.turn) === Number(log.turn)).map((message) => (
+                  <ListItem key={message.frame_number}>{message.message}</ListItem>
+                )))
+              }
+            </OrderedList>
+          </Box>
+        ))}
+      </Chrono>
+    </>
   );
 };
 
